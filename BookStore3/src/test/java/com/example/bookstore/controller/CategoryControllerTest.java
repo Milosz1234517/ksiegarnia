ec2badd519ae63f718b1491d9cb9f800dfdabb30
MILOSZ\milro <mil.rom96@gmail.com>
2023-01-14 14:16:34 +0100

    add category test class

--- BookStore3/src/test/java/com/example/bookstore/controller/CategoryControllerTest.java
+++ BookStore3/src/test/java/com/example/bookstore/controller/CategoryControllerTest.java
@@ -1,38 +1,20 @@
 package com.example.bookstore.controller;
 
-import com.example.bookstore.jwt.AuthTokenFilter;
-import com.example.bookstore.jwt.JwtUtils;
-import com.example.bookstore.repository.BookHeaderRepository;
-import com.example.bookstore.repository.PublishingHouseRepository;
-import com.example.bookstore.service.userDetailsService.UserDetailsServiceImpl;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 
-import javax.servlet.http.HttpServletResponse;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 @WithMockUser
 class CategoryControllerTest {
 
-    private MockMvc mockMvc;
     @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    @Before("")
-    public void setUp() {
-        this.mockMvc = webAppContextSetup(webApplicationContext).build();
-    }
+    private MockMvc mockMvc;
 
     @Test
     void getCategories() {
